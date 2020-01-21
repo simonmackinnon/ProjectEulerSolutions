7c6caab43e82f418dabeaa67cbb64306fc364eba
Simon Mackinnon <simon.x.mackinnon@nab.com.au>
2020-01-22 09:38:04 +1100

    added some solutions

---
+++ SpecialPythagoreanTriplet/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SpecialPythagoreanTriplet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int found = 0;
+
+            long cSquared;
+            double c = 0;
+
+            for(a = 1; a < 1000; a++)
+            {
+                for(b = 1; b < 1000; b++)
+                {
+                    cSquared = (a*a)+(b*b);
+                    c = Math.Sqrt((double)cSquared);
+
+                    if(a+b+c == 1000)
+                    {
+                        found = 1;
+                        break;
+
+                    }
+
+
+                }
+                if(found != 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Pythagorean Triplet you are looking for is " + a + ", " + b + ", " + c);
+            Console.WriteLine("Product abc " + a*b*c);
+        }
+    }
+}
---
+++ SummationOfPrimes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SummationOfPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 17; //sum of 2+3+5+7
+            int number = 11; 
+
+            while(number < 2000000)
+            {
+                if(isPrime(number))
+                {
+                    Console.WriteLine(number + " is prime");
+                    sum+=number;
+                }
+                number+=2;
+            }
+            Console.WriteLine("Sum of primes below 2M: " + sum);
+        }
+
+        static bool isPrime(int num)
+        {
+            int c;
+            for ( c = 3 ; c <= num - 1 ; c+=2 )
+            {
+                if ( num%c == 0 )return false;
+            }
+            if ( c == num ) return true;
+
+            return true;
+        }
+
+    }
+}
