d228bf9ebcb6640bd47972cd92babd380d466b61
Simon Mackinnon <simon.mackinnon15@gmail.com>
2020-01-24 13:27:31 +1100

    some solutions

--- SummationOfPrimes/Program.cs
+++ SummationOfPrimes/Program.cs
@@ -6,32 +6,19 @@ namespace SummationOfPrimes
     {
         static void Main(string[] args)
         {
-            int sum = 17; //sum of 2+3+5+7
-            int number = 11; 
+            long sum = 0; 
+            int number = 0; 
 
             while(number < 2000000)
             {
-                if(isPrime(number))
+                if(PrimeTool.IsPrime(number))
                 {
                     Console.WriteLine(number + " is prime");
                     sum+=number;
                 }
-                number+=2;
+                number++;
             }
             Console.WriteLine("Sum of primes below 2M: " + sum);
         }
-
-        static bool isPrime(int num)
-        {
-            int c;
-            for ( c = 3 ; c <= num - 1 ; c+=2 )
-            {
-                if ( num%c == 0 )return false;
-            }
-            if ( c == num ) return true;
-
-            return true;
-        }
-
     }
 }
