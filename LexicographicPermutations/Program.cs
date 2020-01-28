ddd9e078084b3545bfe26c43145701cebd713023
Simon Mackinnon <simon.mackinnon15@gmail.com>
2020-01-29 10:45:33 +1100

    NameScores, LexicographicPermutations

---
+++ LexicographicPermutations/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LexicographicPermutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string digits = "0123456789";
+            int n = digits.Length; 
+            List<string> list = new List<string>();
+            PermuteString(digits, 0, n - 1,  ref list);
+        }
+
+        static string SwapChars(string a, int i, int j)
+        {
+            char temp; 
+            char[] charArray = a.ToCharArray(); 
+            temp = charArray[i]; 
+            charArray[i] = charArray[j]; 
+            charArray[j] = temp; 
+            string s = new string(charArray); 
+            return s;
+        }
+
+        static void PermuteString(String str, int l, int r, ref List<string> list)
+        { 
+            if (l == r) 
+            {
+                list.Add(str);
+                int count = list.Count();
+                Console.WriteLine(str + " count: " + count); 
+                if (count >= 1010000)
+                {
+                    list.Sort(); 
+                    string last = list.ElementAt(999999);
+                    Console.WriteLine("the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9 is: " + last);
+                    Console.Read();
+                }
+            } 
+            else 
+            { 
+                for (int i = l; i <= r; i++) 
+                { 
+                    str = SwapChars(str, l, i); 
+                    PermuteString(str, l + 1, r, ref list); 
+                    str = SwapChars(str, l, i); 
+                } 
+            } 
+        }
+    }
+}
---
+++ NameScores/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace NameScores
+{
+    class NameScores
+    {
+        static void Main(string[] args)
+        {         
+            StreamReader streamReader = new StreamReader(@"./names.txt");
+            string text = streamReader.ReadToEnd();
+            streamReader.Close();
+
+            text = text.Replace("\"", string.Empty);
+
+            string[] words = text.Split(',');
+            int[] nameScores = new int[words.Length];
+
+            Quicksort(words, 0, words.Length - 1);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                nameScores[i] = (i+1) * scoreForString(words[i]);
+            }
+
+            ulong total = 0;
+
+            foreach (int i in nameScores)
+            {
+                total += (ulong)i;
+            }
+
+            Console.WriteLine ("total: " + total);
+        }
+
+        private static void Quicksort(IComparable[] elements, int left, int right)
+        {
+            int i = left, j = right;
+            IComparable pivot = elements[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (elements[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (elements[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    // Swap
+                    IComparable tmp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (left < j)
+            {
+                Quicksort(elements, left, j);
+            }
+
+            if (i < right)
+            {
+                Quicksort(elements, i, right);
+            }
+        }
+        
+        private static int scoreForString(string str)
+        {
+            int ret = 0;
+            foreach (char c in str)
+            {
+                ret += c - 64;
+            }
+            return ret;
+        }
+
+    }
+}
