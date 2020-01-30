a3df48557dca3dcd6bd91850f55f6af14f6f522f
Simon Mackinnon <simon.mackinnon15@gmail.com>
2020-01-31 08:35:22 +1100

    DistinctPowers

---
+++ DistinctPowers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace DistinctPowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<BigInteger> distinctPowers = new HashSet<BigInteger>();
+            for (int a = 2; a <= 100; a++) 
+            {
+                for (int b = 2; b <= 100; b++) 
+                {
+                    distinctPowers.Add(new BigInteger(Math.Pow(a, b)));
+                }
+            }
+
+            Console.WriteLine("Count of set: " + distinctPowers.Count);
+        }
+    }
+}
