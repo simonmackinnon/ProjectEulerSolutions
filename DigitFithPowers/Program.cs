fbebf2387fb9108736ce5dcbd62bf801e9fd0b46
Simon Mackinnon <simon.mackinnon15@gmail.com>
2020-01-31 08:47:03 +1100

    DigitFithPowers

---
+++ DigitFithPowers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DigitFifthPowers
+{
+    class CDigitFifthPowers
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            int sumOfAll = 0;
+            while (true)
+            {
+                string str = i.ToString();
+                double sumOfDigitFithPowers = 0;
+                for (int j = 0; j < str.Length; j++)
+                {
+                    sumOfDigitFithPowers += Math.Pow((str[j] - 48), 5);
+                }
+                if (sumOfDigitFithPowers == i)
+                {
+                    sumOfAll += (int)sumOfDigitFithPowers;
+                }
+                i++;
+                if (i == 10000000) break;
+            }
+            Console.WriteLine("Sum Of All: " + sumOfAll);
+        }
+    }
+}
