20cf7f22a82f893ad77ed2d17366eb35b1b1778b
simon mackinnon <simon.mackinnon15@gmail.com>
2020-02-20 20:36:57 +1100

    ChampernownesConstant

---
+++ ChampernownesConstant/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+
+namespace ChampernownesConstant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("", 1000000);
+            int i = 1;
+            while(sb.Length < 1000000)
+            {
+                sb.Append(i.ToString());
+                i++;
+            }
+
+            int product = int.Parse(sb[0].ToString());
+            int index = 999999;
+            while(index > 0)
+            {
+                product *= int.Parse(sb[index].ToString());
+                index /= 10;
+            }
+
+            Console.WriteLine("product: {0}", product);
+        }
+    }
+}
