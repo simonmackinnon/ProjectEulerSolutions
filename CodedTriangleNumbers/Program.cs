2fad4fe2b26d001759e85e3547ca8f6ebee26f0b
Simon Mackinnon <simon.mackinnon15@gmail.com>
2020-02-21 12:09:11 +1100

    CodedTriangleNumbers

---
+++ CodedTriangleNumbers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodedTriangleNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("words.txt");  
+
+            var words_array = text.Replace("\"", string.Empty).Split(',');
+
+            var words = new List<string>(words_array);
+
+            int count = words.Where(x => IsWordTriangular(x)).Count();
+
+            Console.WriteLine("Count of Triangular Words in file: {0}", count);
+        }
+
+        static bool IsWordTriangular(string word)
+        {
+            var word_chars = word.ToCharArray();
+            int sum = 0;
+            foreach (char c in word_chars)
+            {
+                int index = char.ToUpper(c) - 64;
+                sum += index;
+            }
+
+            int currentTriagSeqNumber = 1;
+            int jump = 2;
+            while (sum >= currentTriagSeqNumber)
+            {
+                if (sum == currentTriagSeqNumber)
+                {
+                    return true;
+                }
+                currentTriagSeqNumber += jump;
+                jump++;
+            }
+            return false;
+        }
+    }
+}
